42f0c396ecb8af27774990d8b4a86b160b33d2a4
Carlosescalantee <carlos.escalante@tecsup.edu.pe>
2018-03-17 00:16:22 -0500

    laboratorio01

---
+++ lab01/tarealab01/Inicio.js
@@ -0,0 +1,67 @@
+
+var http = require('http');
+
+
+fs = require('fs');
+
+http.createServer(function(solicitud,respuesta){
+	if (solicitud.url === '/index.html') {
+		fs.readFile('./index.html',function(error,html){
+		console.log('Conexion entrante');
+
+
+		respuesta.write(html);
+		respuesta.end();
+	});
+
+
+	}else
+	if (solicitud.url === '/Nosotros.html') {
+		fs.readFile('./Nosotros.html',function(error,html){
+		console.log('Conexion entrante');
+
+
+		respuesta.write(html);
+		respuesta.end();
+	});
+
+
+	}else
+	if (solicitud.url === '/Nuestrosservicios.html') {
+		fs.readFile('./Nuestrosservicios.html',function(error,html){
+		console.log('Conexion entrante');
+
+
+		respuesta.write(html);
+		respuesta.end();
+	});
+
+
+	}else
+	if (solicitud.url === '/Catalogoclientes.html') {
+		fs.readFile('./Catalogoclientes.html',function(error,html){
+		console.log('Conexion entrante');
+
+
+		respuesta.write(html);
+		respuesta.end();
+	});
+
+
+	}else
+	if (solicitud.url === '/Contacto.html') {
+		fs.readFile('./Contacto.html',function(error,html){
+		console.log('Conexion entrante');
+
+
+		respuesta.write(html);
+		respuesta.end();
+
+
+	});
+
+
+	}
+
+}).listen(8080);
+console.log('Pagina Web');
